5de0c3b7cfa9ea7c31d1640073aa6baba2d7c54c
Kan Ninomiya <ninomiyakan.dev@gmail.com>
2023-12-30 20:11:06 +0900

    update use dlib

--- main.py
+++ main.py
@@ -1,46 +1,72 @@
-from PIL import Image
-import numpy as np
+import dlib
 import cv2
 
-# Load the image file
-img_path = '/mnt/data/IMG_7361.JPG'
-original_image = Image.open(img_path)
-
-# Convert to an array
-image_array = np.array(original_image)
-
-# Use OpenCV to load the image for face detection
-cv_image = cv2.cvtColor(image_array, cv2.COLOR_RGB2BGR)
+# Load the image in grayscale
 gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
 
-# Load the default face detector
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+# Initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor using the path to the dat file
+# which is assumed to be in the working directory.
+predictor_path = "./shape_predictor_68_face_landmarks.dat"
 
-# Detect faces
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+# The facial landmark predictor is required to be in the same directory as this script.
+# As internet access is not available, we will attempt to use the file if it is present,
+# but this step is expected to fail since we cannot download the predictor data file.
+try:
+    # Create the facial landmark predictor
+    predictor = dlib.shape_predictor(predictor_path)
 
-# For the sake of this example, we'll only focus on the first detected face
-for (x, y, w, h) in faces:
-    # Let's make the face 10% smaller
-    shrink_factor = 0.9
-    new_w = int(w * shrink_factor)
-    new_h = int(h * shrink_factor)
+    # Now that we have the detector and predictor, we can process the image:
+    for (x, y, w, h) in faces:
+        # Create a dlib rectangle object from the bounding box coordinates
+        rect = dlib.rectangle(left=x, top=y, right=x+w, bottom=y+h)
 
-    # Calculate the offset needed to center the smaller face
-    offset_w = int((w - new_w) / 2)
-    offset_h = int((h - new_h) / 2)
+        # Use the predictor to find the landmark points
+        landmarks = predictor(gray, rect)
 
-    # Create a new image with a smaller face
-    face_img = cv_image[y + offset_h:y + h - offset_h, x + offset_w:x + w - offset_w]
-    face_img = cv2.resize(face_img, (w, h))
+        # Convert landmarks to numpy array
+        points = np.array([[p.x, p.y] for p in landmarks.parts()])
 
-    # Put the resized face back into the original image
-    cv_image[y:y+h, x:x+w] = face_img
+        # Define a mask for the face which is the same size as the original image
+        mask = np.zeros_like(cv_image, dtype=np.uint8)
 
-# Convert back to RGB and save the edited image
-edited_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
-edited_pil_image = Image.fromarray(edited_image)
-edited_img_path = '/mnt/data/edited_IMG_7361.JPG'
-edited_pil_image.save(edited_img_path)
+        # We will use convex hull to create a polygon around the landmarks
+        hull = cv2.convexHull(points)
+        cv2.fillConvexPoly(mask, hull, (255, 255, 255))
 
-edited_img_path
+        # Create a Delaunay triangulation of the points
+        rect = cv2.boundingRect(hull)
+        subdiv = cv2.Subdiv2D(rect)
+        for p in points:
+            subdiv.insert((int(p[0]), int(p[1])))
+        triangles = subdiv.getTriangleList()
+        triangles = np.array(triangles, dtype=np.int32)
+
+        # For each triangle, apply a local warp to make the face smaller
+        for t in triangles:
+            pts1 = np.float32([t[0:2], t[2:4], t[4:6]])
+            # Compute the centroid of the triangle
+            center = np.mean(pts1, axis=0)
+            # Move the vertices of the triangle towards the centroid to make the face smaller
+            pts2 = np.float32([center + (v - center) * shrink_factor for v in pts1])
+            # Apply the warp to a small region within the triangle
+            cv2.warpAffine(cv_image, cv2.getAffineTransform(pts1, pts2),
+                           (cv_image.shape[1], cv_image.shape[0]),
+                           dst=mask,
+                           borderMode=cv2.BORDER_REFLECT,
+                           flags=cv2.WARP_INVERSE_MAP)
+
+        # Blend the masked small face with the original image
+        seamless = cv2.seamlessClone(mask, cv_image, mask, (x+w//2, y+h//2), cv2.NORMAL_CLONE)
+
+    # Save the result
+    result = cv2.cvtColor(seamless, cv2.COLOR_BGR2RGB)
+    result_pil = Image.fromarray(result)
+    result_img_path = '/mnt/data/seamless_IMG_7361.JPG'
+    result_pil.save(result_img_path)
+
+except Exception as e:
+    # If any error occurs, we'll just return a message
+    result_img_path = f"Error: {str(e)}"
+
+result_img_path
